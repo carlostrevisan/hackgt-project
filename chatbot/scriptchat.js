56aa077454278bd9ff660122a81f83f232667bd3
Carlos Trevisan <carlosedutrevisan@gmail.com>
2022-10-22 13:26:16 -0400

    Update scriptchat.js

--- chatbot/scriptchat.js
+++ chatbot/scriptchat.js
@@ -1,15 +1,31 @@
 const sendButton = document.getElementById("sendMessage")
 
 sendButton.addEventListener("click", function () {
+    sendInputMsg()
+})
+
+document.getElementById("inputMessage").addEventListener("keyup", function (event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        document.getElementById("sendMessage").click();
+    }
+});
+
+function sendInputMsg() {
     let message = document.getElementById("inputMessage").value
+    let div = document.createElement("div")
+    div.style = "height: 50px;"
 
     if (message != "") {
         let msg = document.createElement("button")
         msg.classList.add("btn")
         msg.classList.add("btn-light")
         msg.innerHTML = message;
+        msg.style = "float: right;"
         let msgDiv = document.getElementById("chatboxDiv")
-
-        msgDiv.append(msg);
+        div.append(msg)
+        msgDiv.append(div);
     }
-})
+
+    document.getElementById("inputMessage").value = "";
+}
